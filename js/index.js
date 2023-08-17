234aab31723f3bffd952b7bf30042d978ce36b4a
seokyoungjin <106298467+seokyoungjin@users.noreply.github.com>
2023-08-17 16:52:11 +0900

    o

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
-$(document).ready(function(){
-  $('.your-class').slick({
+$(function(){
+  $('.slider-wrap').slick({
+    slide:'div', 
+    infinite : true,
+    slidesToShow : 2,
+    slidesToScroll : 1,
+    speed : 500,
+    arrows :true ,
+    dots :true,
+    autoplay :true,
+    autoplaySpeed : 2000,
+    pause0nHover : true,
+    vertical : false,
+    prevArrow : "<buttno type='button' class='slick-prev'>Previous</button>",
+    nextArrow : "<button type='button' class='slick-next'>Next</button>",
+    draggable : true,
   });
-});
+})
