ddbcf75497e510ed9cc533da5852c4127462c7ba
seokyoungjin <106298467+seokyoungjin@users.noreply.github.com>
2023-08-22 17:39:14 +0900

    p

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
-$(function(){
-  $('.slider-wrap').slick({
-    slide:'div', 
-    infinite : true,
-    slidesToShow : 2,
-    slidesToScroll : 1,
-    speed : 500,
-    arrows :true,
-    dots :true,
-    autoplay :true,
-    autoplaySpeed : 2000,
-    pause0nHover : true,
-    vertical : false,
-    prevArrow : "<buttno type='button' class='slick-prev'>Previous</button>",
-    nextArrow : "<button type='button' class='slick-next'>Next</button>",
-    draggable : true,
-  });
-})
+/* text_iife.js */
+// 텍스트 작성과 삭제 즉시 실행 함수
+(function(){
+  const spanEl = document.querySelector("main h2 span");
+  const txtArr = ['Web Publisher', 'Front-End Developer', 'Web UI Designer', 'UX Designer', 'Back-End Developer'];
+  let index = 0;
+  let currentTxt = txtArr[index].split("");
+  function writeTxt(){
+    spanEl.textContent  += currentTxt.shift(); 
+    if(currentTxt.length !== 0){ 
+      setTimeout(writeTxt, Math.floor(Math.random() * 100));
+    }else{
+      currentTxt = spanEl.textContent.split("");
+      setTimeout(deleteTxt, 3000);
+    }
+  }
+  function deleteTxt(){
+    currentTxt.pop();
+    spanEl.textContent = currentTxt.join("");
+    if(currentTxt.length !== 0){
+      setTimeout(deleteTxt, Math.floor(Math.random() * 100))
+    }else{
+      index = (index + 1) % txtArr.length;
+      currentTxt = txtArr[index].split("");
+      writeTxt();
+    }
+  }
+  writeTxt();
+})();
